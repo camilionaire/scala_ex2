7bce847e69b1ac56eb0ad1ad218ca48bbe55e421
Camilo Schaser-Hughes <camilo3@pdx.edu>
2022-10-08 12:04:10 -0700

    added the start of a compiler test

---
+++ TestBLC.scala
@@ -0,0 +1,36 @@
+//-------------------------------------------------------------------------
+// Name: Camilo Schaser-Hughes
+// Class: CS558 Programming Languages, Prof: Jinke Li
+// Date: October 8th, 2022
+// Description: Test program that is based off of interpreter test
+// Tests the methods of the BLComp class.
+//
+// Supporting code for CS558 Programming Languages. 
+// Dept of Computer Science, Portland State University
+// J. Li (8/2022) 
+//-------------------------------------------------------------------------
+
+// Testing BoolLang compiler
+//
+import org.scalatest.FunSuite
+import BLComp._
+
+class TestBLC extends FunSuite {
+
+  test("correctly compile simple expressions" {
+
+//    assertResult(
+
+
+  }
+
+//  test("correctly interpret simple exprs") {
+//  assertResult(false)(BLInterp("(not (and t (not f)))"))
+//  assertResult(true)(BLInterp("(and (or (or f f) t) t)"))
+//  assertResult(true)(BLInterp("(or (or (and f f) t) f)"))
+//  assertResult(true)(BLInterp("(and (xor t f) (xor f t))"))
+//  assertResult(true)(BLInterp("(xor (and t f) (or f t))"))
+//}
+
+}
+
